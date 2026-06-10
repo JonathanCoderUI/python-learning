77d55453bfa14ce73c0657c00d6db1feb4ce928b
Jonathan Kana <jokana2008@gmail.com>
2026-06-10 15:08:45 +0000

    Belajar Escape Character dan Sequence

---
+++ day-lima.py
@@ -0,0 +1,22 @@
+course = "Python for Everybody"
+print(course)
+print("-------------------------------")
+# hari ini belajar Escape Sequence
+# contohnya adalah kalau kita mau buat new line, kita bisa pakai \n
+print("Hello World!\nWelcome to Python Programming")
+print("-------------------------------")
+# kalau kita mau buat ada tab antar kata, kita bisa pakai \t
+print("Hello\tWorld!\tWelcome to Python Programming")
+print("-------------------------------")
+# kalau kita mau buat ada backslash di dalam kalimat, kita bisa pakai \\
+print("Hello\\World!\\Welcome to Python Programming")
+print("-------------------------------")
+# kalau kita mau buat ada single quote, kita bisa pakai \'
+print('Hello\'s World!\'s Welcome to Python Programming')
+print("-------------------------------")
+# kalau kita mau buat ada double quote, kita bisa pakai \"
+print("Hello\"s World!\"s Welcome to Python Programming")
+print("-------------------------------")
+# kalau kita mau buat backspace kayak delete jeda antar kata, kita bisa pakai \b
+print("Hello\bWorld!\bWelcome to Python Programming")
+print("-------------------------------")
